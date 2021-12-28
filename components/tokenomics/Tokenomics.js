bd08b9d448d78ee4f651423fa448c2befe51c76c
zealotdev <g33ky.me@pm.me>
2021-12-28 13:57:56 +0300

    🔨: Tokenomics details added

--- components/tokenomics/Tokenomics.js
+++ components/tokenomics/Tokenomics.js
@@ -1,11 +1,57 @@
 import tw, { css } from 'twin.macro';
 import 'chart.js/auto';
 import { Pie } from 'react-chartjs-2';
+import { useState } from 'react';
 
 export default function Tokenomics() {
+  const [copied, setCopied] = useState(false);
+  const address = '0x282362dcd3df51a68c13a6402446c39ec63c350e';
+
   return (
     <div css={tw`mt-20 md:mt-32`} id="tokenomics">
       <h3 css={tw`text-gray-500 font-mono text-2xl`}>Tokenomics.</h3>
+      {/* Contract address */}
+      <div css={tw`mx-auto flex flex-col text-center justify-center`}>
+        <h4 css={tw`text-gray-400 font-mono md:text-sm`}>Contract Address</h4>
+        <div css={tw`flex flex-col md:relative w-full md:w-4/6 mx-auto`}>
+          <input
+            css={tw`py-1 md:py-2 px-2 md:px-4 text-xs md:text-lg w-full text-center rounded relative border-0 text-yellow-600`}
+            className="copy-body"
+            type="text"
+            value={address}
+          />
+          <button
+            css={tw`block mt-2 md:mt-0 md:absolute md:right-2 md:top-1/4 rounded border-0 p-1 bg-yellow-600 text-gray-200 cursor-pointer active:bg-yellow-400 active:text-gray-600`}
+            onClick={() => {
+              navigator.clipboard.writeText(address);
+              setCopied(true);
+            }}
+          >
+            {copied ? 'copied' : 'copy'}
+          </button>
+        </div>
+      </div>
+      {/* Summary */}
+      <div css={tw`md:flex md:flex-col mx-auto md:w-4/6`}>
+        <div css={tw`text-gray-400 md:flex md:w-full justify-between`}>
+          <h5 css={tw`font-mono`}>
+            Name: <span css={tw`text-yellow-600 font-bold`}>GONG</span>
+          </h5>
+          <h5 css={tw`font-mono`}>
+            Symbol: <span css={tw`text-yellow-600 font-bold`}>GONG</span>
+          </h5>
+        </div>
+        <div css={tw`text-gray-400 md:flex md:w-full justify-between`}>
+          <h5 css={tw`font-mono`}>
+            Supply:{' '}
+            <span css={tw`text-yellow-600 font-bold`}>100,000,000,000</span>
+          </h5>
+          <h5 css={tw`font-mono`}>
+            Decimal: <span css={tw`text-yellow-600 font-bold`}>9</span>
+          </h5>
+        </div>
+      </div>
+      {/* Roadmap */}
       <div>
         <div
           className="roadmap-content"
